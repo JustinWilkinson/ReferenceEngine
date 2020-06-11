abc13f81f36ef620797d558de118d856eb4b15f3
Justin Wilkinson <justin_wilkinson@talktalk.net>
2020-06-11 16:29:19 +0100

    Add tests and small tidy

---
+++ Test/Bibtex/Abstractions/BibtexEntryTest.cs
@@ -0,0 +1,56 @@
+﻿using Bibtex.Abstractions;
+using Bibtex.Abstractions.Entries;
+using Bibtex.Abstractions.Fields;
+using Bibtex.Enumerations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Test.Bibtex.Abstractions
+{
+    [TestFixture]
+    public class BibtexEntryTest
+    {
+        [Test]
+        public void ApplyStyle_NullEntryStyle_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => GetEntry().ApplyStyle(null));
+        }
+
+        [Test]
+        public void ApplyStyle_ValidEntryStyle_FormatsOutputSuccessfully()
+        {
+            // Arrange
+            var entry = GetEntry();
+            var style = new EntryStyle
+            {
+                Fields = new List<Field>
+                {
+                    new EntryField { Value = "Title" },
+                    new ConstantField { Value = "by" },
+                    new EntryAuthorField { Format = OutputAuthorFormat.Default },
+                    new ConstantField { Value = "in" },
+                    new EntryField { Value = "Journal", Italic = true, Suffix = "," },
+                    new EntryField { Value = "Year", Suffix = "." }
+                }
+            };
+
+            // Act
+            var result = entry.ApplyStyle(style);
+
+            // Assert
+            Assert.AreEqual("A Really Awesome Article by A. N. Author in \\emph{Some Really Good Journal}, 2020.", result);
+        }
+
+        public BibtexEntry GetEntry()
+        {
+            return new BibtexEntry(EntryType.Article, "Article1", new Dictionary<string, string> 
+            {
+                { "Author", "A. N. Author" },
+                { "Title", "A Really Awesome Article" },
+                { "Year", "2020" },
+                { "Journal", "Some Really Good Journal" }
+            });
+        }
+    }
+}
---
+++ Test/Bibtex/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using Bibtex.Extensions;
+using NUnit.Framework;
+
+namespace Test.Bibtex.Extensions
+{
+    [TestFixture]
+    public class StringExtensionsTest
+    {
+        [TestCase("{ Some value here }", ExpectedResult = "Some value here")]
+        [TestCase("{ Some value here \t \r \n}", ExpectedResult = "Some value here")]
+        [TestCase(@"{ "" Some value here ""}", ExpectedResult = "Some value here")]
+        public string TrimIgnoredCharacters_RemovesIgnoredBibtexCharacters_Successful(string toTrim)
+        {
+            return toTrim.TrimIgnoredCharacters();
+        }
+
+        [TestCase("{Some value here}", ExpectedResult = "Some value here")]
+        [TestCase("{Some value with {nested value here}}", ExpectedResult = "Some value with nested value here")]
+        public string ReplaceBraces_RemovesBraces_Successfully(string toTrim)
+        {
+            return toTrim.ReplaceBraces();
+        }
+    }
+}
