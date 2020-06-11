be3705a2735749caf98d50197e89681227cf2b06
Justin Wilkinson <justin_wilkinson@talktalk.net>
2020-06-11 17:21:22 +0100

    Add new async extension and unit test

---
+++ Test/LatexReferences/Helpers/TestDb.cs
@@ -0,0 +1,13 @@
+﻿using Bibtex.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test.LatexReferences.Helpers
+{
+    public class TestDb : ApplicationDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseInMemoryDatabase("Test");
+        }
+    }
+}
